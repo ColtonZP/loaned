c684d986f02e80cf11666b02d952bf41cfcbd21d
Colton Pemberton <czpemberton@gmail.com>
2020-05-13 19:14:05 -0700

    moved to mobx

--- src/App.js
+++ src/App.js
@@ -1,37 +1,48 @@
 import React, { useState } from 'react';
+import { inject, observer } from 'mobx-react';
+
 import { inspectText } from './function';
+import './App.scss';
+import Person from './Person';
 
-import './App.css';
+const App = inject('OwedStore')(
+  observer(props => {
+    const [input, changeInput] = useState('');
+    const { OwedStore } = props;
+    const records = OwedStore.records;
+    console.log(records);
 
-function App() {
-  const [input, changeInput] = useState('');
-  const [history, rewriteHistory] = useState([]);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    const inspectedText = inspectText(input);
-    if (inspectedText.status === 'failed') {
-      alert('a(n) ' + inspectedText.reason + ' appears to be missing.');
-    } else {
-      rewriteHistory([...history, input]);
-      changeInput('');
+    function handleSubmit(e) {
+      e.preventDefault();
+      const inspectedText = inspectText(input);
+      if (inspectedText.status === 'failed') {
+        alert('a(n) ' + inspectedText.reason + ' appears to be missing.');
+      } else {
+        OwedStore.addRecord(inspectedText);
+        changeInput('');
+      }
     }
-  }
 
-  return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name=""
-          id=""
-          value={input}
-          onChange={e => changeInput(e.target.value)}
-        />
-        <input type="submit" value="submit" />
-      </form>
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <form onSubmit={e => handleSubmit(e)}>
+          <input
+            type="text"
+            name=""
+            id=""
+            value={input}
+            onChange={e => changeInput(e.target.value)}
+          />
+          <input type="submit" value="submit" />
+        </form>
+        <div className="people">
+          {OwedStore.records.map(person => (
+            <Person person={person} key={person.name} />
+          ))}
+        </div>
+      </div>
+    );
+  })
+);
 
 export default App;
