db2c5566180473d24fab15a0525f8ea4cd213017
Colton <czpemberton@gmail.com>
2021-01-13 11:34:26 -0800

    display person history

--- src/components/People.tsx
+++ src/components/People.tsx
@@ -3,12 +3,23 @@ import React, { useContext } from 'react';
 import { useStore } from '../store';
 import { Person } from './Person';
 
+type Record = {
+  name: string;
+  amount: number;
+  history: History[];
+};
+
+type History = {
+  amount: number;
+  change: string;
+};
+
 export const People = () => {
   const records: any = useStore(state => state.records);
 
   return (
     <ul>
-      {records.map((person: { amount: any; name: string }) => (
+      {records.map((person: Record) => (
         <Person key={person.name} value={person} />
       ))}
     </ul>
--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -4,15 +4,29 @@ type Props = {
   value: {
     name: string;
     amount: number;
+    history: History[];
   };
 };
 
+type History = {
+  amount: number;
+  change: string;
+};
+
 export const Person = ({ value }: Props) => {
-  const { name, amount } = value;
+  const { name, amount, history } = value;
 
   return (
     <li>
-      <span>{`${name} ${amount}`}</span>
+      <span className="Name">{name}</span>
+      <span className="Amount">{amount.toFixed(2)}</span>
+      <ul>
+        {history.map((record: History) => (
+          <li className={record.change} key={record.amount}>
+            {record.amount}
+          </li>
+        ))}
+      </ul>
     </li>
   );
 };
